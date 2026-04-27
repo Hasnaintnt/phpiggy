af34c0b6fe10e664dd4a7ceb05b836fbbfc6aa37
Muhammad Hasnain <161151300+Hasnaintnt@users.noreply.github.com>
2026-04-27 14:16:40 +0500

    worthy update

---
+++ src/App/Controllers/AboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+Namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
+class AboutController{
+    private TemplateEngine $view;
+
+    public function __construct(){
+        $this->view = new TemplateEngine(paths::VIEW);
+    }
+    public function about(){
+        echo $this->view->render("about.php",[
+            'title' => "About PHPiggy",
+            'dangerousData' => '<script> alert(123) </script>'
+        ]);
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController,AboutController};
 
 require __DIR__ . "/../App/functions.php";
 
@@ -12,5 +12,7 @@ require __DIR__ . "/../../vendor/autoload.php";
 $app = new App();
 
 $app->get("/",[HomeController::class,"home"]);
+
+$app->get("/about",[AboutController::class,"about"]);
 
 return $app;
